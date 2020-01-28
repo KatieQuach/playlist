699efc229ba3ed73f244181a4d2c27702d415f60
KatieQuach <2021097@icacademy.org>
2020-01-28 02:27:08 +0000

    Cleaned it up a bit and able to display

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-let songArray = [""]
+var obj = {
+    Artist_Name: "Ariana Grande",
+    Song_Title: "Bloodline",
+    Duration: "2:41",
+    Link:"dfaa",
+    Image: "asda",
 
-songArray.forEach(function(guest) {
-    $(".displaySongs").append(songNames)
+}
+
+let playlist = [obj]
+
+
+$(".addButton").click(function(){
+    playlist.forEach(function(song){
+        $(".displaySongs").append("<p> Artist_Name: "+song["Artist_Name"]+"</p>");
+        $(".displaySongs").append("<p> Song_Title: "+song["Song_Title"]+"</p>");
+        $(".displaySongs").append("<p> Duration: "+song["Duration"]+"</p>");
+        $(".displaySongs").append("<p> Link: "+song["Link"]+"</p>");
+        $(".displaySongs").append("<p> Image: "+song["Image"]+"</p>");
 });
+//match 
+
 //working click handler that stores information (idk about retrieving?)user input
 songArray.push(newSong);
-$("addButton").click(function() {
-    let newSongPicture = $(".songPicture").val();
-    let newSongName = $(".songName").val();
-    let newSongDuration = $(".songDuration").val();
-    let newSongUrl = $("songLink").val();
+    $("addButton").click(function() {
+        let newSong_Title = $(".song").val();
+        let newSong_Title = $(".song").val();
+        
+        let newSongPicture = $(".songPicture").val();
+        let newSongName = $(".songName").val();
+        let newSongDuration = $(".songDuration").val();
+        let newSongUrl = $("songLink").val();
+
+    });
 //pushes new items to song arrays + displays the result to the screen
   songArray.push(newSong);
   //append or display
